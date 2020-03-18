5fc5621c3107f926223972998c530d9a34ad370d
Your Name <sudhanshu.thethunder@gmail.com>
2020-03-18 21:56:01 +0530

    Class to create/change Shared Preferences added

---
+++ app/src/main/java/com/example/android/examshield/PrefUtils.java
@@ -0,0 +1,20 @@
+package com.example.android.examshield;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+public class PrefUtils {
+    private static final String PREF_KIOSK_MODE = "pref_kiosk_mode";
+
+
+    public static boolean isKioskModeActive(final Context context) {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
+        return sp.getBoolean(PREF_KIOSK_MODE, false); //sets false if key not found
+    }
+
+    public static void setKioskModeActive(final boolean active, final Context context) {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
+        sp.edit().putBoolean(PREF_KIOSK_MODE, active).commit();
+    }
+}
