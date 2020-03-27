11da4583a95fb263aa7faafedccce4a8dc748fab
Sudhanshu Dubey <sudhanshu.thethunder@gmail.com>
2020-03-27 22:00:17 +0530

    Scope of the class updated

--- app/src/main/java/com/example/android/examshield/PrefUtils.java
+++ app/src/main/java/com/example/android/examshield/PrefUtils.java
@@ -4,17 +4,17 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
-public class PrefUtils {
+class PrefUtils {
     private static final String PREF_KIOSK_MODE = "pref_kiosk_mode";
 
 
-    public static boolean isKioskModeActive(final Context context) {
+    static boolean isKioskModeActive(final Context context) {
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
         return sp.getBoolean(PREF_KIOSK_MODE, false); //sets false if key not found
     }
 
-    public static void setKioskModeActive(final boolean active, final Context context) {
+    static void setKioskModeActive(final boolean active, final Context context) {
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
-        sp.edit().putBoolean(PREF_KIOSK_MODE, active).commit();
+        sp.edit().putBoolean(PREF_KIOSK_MODE, active).apply();
     }
 }
