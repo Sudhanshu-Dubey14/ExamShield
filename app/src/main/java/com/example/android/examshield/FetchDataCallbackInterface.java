35fff990b147139baa86dffa9e3de87c7a4e0a90
ShivCharanSharma <shivcharanmt@gmail.com>
2020-04-25 17:49:24 +0530

    this is interface to callback function of response received from server

--- app/src/main/java/com/example/android/examshield/FetchDataCallbackInterface.java
+++ app/src/main/java/com/example/android/examshield/FetchDataCallbackInterface.java
@@ -1,4 +1,8 @@
 package com.example.android.examshield;
-
+/**
+ * Interface defining a callable to be used as callback when fetching server data
+ */
 public interface FetchDataCallbackInterface {
+    // method called when server's data get fetched
+    public void fetchDataCallback (String result);
 }
